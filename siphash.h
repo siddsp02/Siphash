612b6aa15fb0987ad1434f1f2452b5bd8b404c22
Sidd <sidd.s.pai@gmail.com>
2023-11-13 01:36:54 -0700

    Added header guard.

--- siphash.h
+++ siphash.h
@@ -1,3 +1,5 @@
+#ifndef SIPHASH_H
+#define SIPHASH_H
 #include <stdio.h>
 #include <stdint.h>
 #include "array.h"
@@ -8,3 +10,4 @@
 typedef uint64_t uint128_t[2];
 
 uint64_t siphash(const uint128_t k, array(char) *m);
+#endif /* SIPHASH_H */
